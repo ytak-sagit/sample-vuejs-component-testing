7ab0a3ae7c8b96491dd57cd6d33704d6bc70af1b
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-08-23 21:09:30 +0900

    refactor: ボトムシート表示を`SamplePage`へ移動

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
 <template>
-  <div>
-    <h1>ボトムシートの例</h1>
-    <button @click="openBottomSheet">ボトムシートを開く</button>
-
-    <BottomSheet ref="bottom-sheet" heading="ボトムシートの内容">
-      <template #content>
-        <p>ここに表示したいコンテンツを入れます。</p>
-      </template>
-    </BottomSheet>
-  </div>
+  <SamplePage />
 </template>
 
 <script setup lang="ts">
-import { useTemplateRef } from 'vue'
-import BottomSheet from './components/BottomSheet.vue'
-
-// BottomSheetコンポーネントへの参照
-const bottomSheetRef = useTemplateRef('bottom-sheet')
-
-// ボトムシートを開くメソッド
-const openBottomSheet = () => {
-  if (bottomSheetRef.value) {
-    bottomSheetRef.value.open()
-  }
-}
+import SamplePage from './components/SamplePage.vue'
 </script>
---
+++ src/components/SamplePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>ボトムシートの例</h1>
+    <button @click="openBottomSheet">ボトムシートを開く</button>
+
+    <BottomSheet ref="bottom-sheet" heading="ボトムシートの内容">
+      <template #content>
+        <p>ここに表示したいコンテンツを入れます。</p>
+      </template>
+    </BottomSheet>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useTemplateRef } from 'vue'
+import BottomSheet from './BottomSheet.vue'
+
+// BottomSheetコンポーネントへの参照
+const bottomSheetRef = useTemplateRef('bottom-sheet')
+
+// ボトムシートを開くメソッド
+const openBottomSheet = () => {
+  if (bottomSheetRef.value) {
+    bottomSheetRef.value.open()
+  }
+}
+</script>
