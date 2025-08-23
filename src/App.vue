d6c563c7330ebf987f3ac162936ff116f4a03c5a
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-08-23 16:00:33 +0900

    feat: ボトムシートを表示できるようにAppを修正

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <h1>ボトムシートの例</h1>
+    <button @click="openBottomSheet">ボトムシートを開く</button>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <BottomSheet ref="bottom-sheet" heading="ボトムシートの内容">
+      <template #content>
+        <p>ここに表示したいコンテンツを入れます。</p>
+      </template>
+    </BottomSheet>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup lang="ts">
+import { useTemplateRef } from 'vue'
+import BottomSheet from './components/BottomSheet.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+// BottomSheetコンポーネントへの参照
+const bottomSheetRef = useTemplateRef('bottom-sheet')
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+// ボトムシートを開くメソッド
+const openBottomSheet = () => {
+  if (bottomSheetRef.value) {
+    bottomSheetRef.value.open()
   }
 }
-</style>
+</script>
